e86e2649bd328805249b44f64603b04b07daee59
Hochart-dev <hochart.dev@gmail.com>
2021-06-28 17:24:29 +0200

    add conf.js / env.sample and first route in index

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { db } = require('./conf');
 
 const app = express();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const sql = 'SELECT * FROM users';
+  const [results] = await db.query(sql);
+  res.json(results);
 });
 
 app.use('/', (req, res) => {
@@ -13,3 +18,5 @@ app.use('/', (req, res) => {
 app.listen(5050, () => {
   console.log('Terra Battle API now available on http://localhost:5050 !');
 });
+
+Envoyer un message à @[Lille] Robin Delannoy
