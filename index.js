376ec34ba07ba7b4f681675b639cfb368de9ffbf
Hacene010 <hamrounehacene06@gmail.com>
2021-06-29 15:03:22 +0200

    added routes to post data to the tables and a route to get the categories

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { db } = require('./conf');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/users', async (req, res) => {
   const sql = 'SELECT * FROM users';
@@ -11,55 +14,73 @@ app.get('/users', async (req, res) => {
   res.json(results);
 });
 
-
 app.get('/users/:id', async (req, res) => {
-  const {id}=req.params;
+  const { id } = req.params;
   const sql = 'SELECT mail,username,contact FROM users WHERE id=?';
-  let sqlValues = [id];
+  const sqlValues = [id];
   const [results] = await db.query(sql, sqlValues);
   res.json(results);
 });
 
-
-
-app.post('/users',async(req,res)=>{
-  const{ mail,username,contact} =req.body;
-  const sql =
-  'INSERT INTO users (mail,username,contact) VALUES(?,?,?)'
-  let sqlValues = [mail,username,contact];
-const[results]= await db.query(sql, sqlValues);
-res.status(201).json(results);
+app.post('/users', async (req, res) => {
+  const { mail, username, contact } = req.body;
+  const sql = 'INSERT INTO users (mail,username,contact) VALUES(?,?,?)';
+  const sqlValues = [mail, username, contact];
+  const [results] = await db.query(sql, sqlValues);
+  res.status(201).json(results);
 });
 
-app.put('/users/:id',async(req,res)=>{
-  const {id}=req.params;
-  const{ mail,username,contact} =req.body;
-  const sql =
-  'UPDATE users SET mail=?, username=?, contact=? WHERE id=?';
-  let sqlValues = [mail,username,contact,id];
-const[results]= await db.query(sql, sqlValues);
-res.status(201).json(results);
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { mail, username, contact } = req.body;
+  const sql = 'UPDATE users SET mail=?, username=?, contact=? WHERE id=?';
+  const sqlValues = [mail, username, contact, id];
+  const [results] = await db.query(sql, sqlValues);
+  res.status(201).json(results);
 });
 
 app.delete('/users/:id', async (req, res) => {
-  const {id}=req.params;
+  const { id } = req.params;
   const sql = 'DELETE FROM users WHERE id=?';
-  let sqlValues = [id];
+  const sqlValues = [id];
   const [results] = await db.query(sql, sqlValues);
   res.json(results);
 });
 
+app.post('/wanted', async (req, res) => {
+  const {
+    id,
+    offer,
 
+    offerTitle,
+    request,
 
+    requestTitle,
+  } = req.body;
+  console.log(req.body);
 
-app.post('/wantedservice', async (req, res) => {
-  const { usersId, categoryId, textWanted } = req.body;
   const sql =
-    'INSERT INTO wantedservice (usersId, categoryId, textWanted) VALUES(?,?,?)';
-  const sqlValues = [usersId, categoryId, textWanted];
+    'INSERT INTO wantedservice (usersId, textWanted, title) VALUES( ?,?,?)';
+  const sqlValues = [id, request, requestTitle];
+  await db.query(sql, sqlValues);
 
-  const [results] = await db.query(sql, sqlValues);
-  res.status(201).json(results);
+  const sql2 =
+    'INSERT INTO proposedservice (usersId, textProposed, title) VALUES( ?,?,?)';
+  const sqlValues2 = [id, offer, offerTitle];
+  await db.query(sql2, sqlValues2, (err, results) => {
+    console.log(err);
+    if (err) res.status(500).send('Error ....');
+    else {
+      console.log(results);
+      res.status(201).json('It worked');
+    }
+  });
+});
+
+app.get('/category', async (req, res) => {
+  const sql = 'SELECT * FROM category';
+  const [results] = await db.query(sql);
+  res.json(results);
 });
 
 app.get('/wantedservice', async (req, res) => {
@@ -83,13 +104,6 @@ app.get('/proposedservice', async (req, res) => {
   const [results] = await db.query(sql);
   res.json(results);
 });
-
-
-
-
-
-
-
 
 app.use('/', (req, res) => {
   res.status(404).send('Route not found! ');
